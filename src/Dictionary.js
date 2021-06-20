2a623be1e19c5f86c21abf60ac534a0e509bf0b8
theresialindholm <80003084+theresialindholm@users.noreply.github.com>
2021-06-20 22:58:52 +0200

    Added photo api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 import axios from "axios";
 
@@ -7,14 +8,23 @@ export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+setPhotos(response.data.photos);
+    }
+
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+        let pexelsApiKey = "563492ad6f917000010000017758e9276ad04065a60288de1cd221d7";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}`}
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -45,6 +55,7 @@ Suggested words: sunset, boat, travel...
     </div>
     </section>
 <Results results={results} />
+<Photos photos={photos} />
     </div>
     );
 } else {
